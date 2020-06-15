6d0080d31e09ae4eb9fd867a8649764aabd462c6
cho881020 <kj_cho@idealidea.co.kr>
2020-06-15 19:46:50 +0900

    Initial commit

---
+++ app/src/main/java/kr/co/tjoeun/apipractice_20200615/BaseActivity.kt
@@ -0,0 +1,12 @@
+package kr.co.tjoeun.apipractice_20200615
+
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity : AppCompatActivity() {
+
+    val mContext = this
+
+    abstract fun setupEvents()
+    abstract fun setValues()
+
+}
---
+++ app/src/main/java/kr/co/tjoeun/apipractice_20200615/LoginActivity.kt
@@ -0,0 +1,22 @@
+package kr.co.tjoeun.apipractice_20200615
+
+import android.os.Bundle
+
+class LoginActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
+}
