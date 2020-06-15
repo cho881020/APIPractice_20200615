f544df4f0e800c81440b6ffd9b280cb542719ca7
cho881020 <kj_cho@idealidea.co.kr>
2020-06-15 20:59:50 +0900

    로그인화면에서 실제로 로그인 API 호출  => 응답 내용은 로그로만 확인중

--- app/src/main/java/kr/co/tjoeun/apipractice_20200615/LoginActivity.kt
+++ app/src/main/java/kr/co/tjoeun/apipractice_20200615/LoginActivity.kt
@@ -2,6 +2,7 @@ package kr.co.tjoeun.apipractice_20200615
 
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_login.*
+import kr.co.tjoeun.apipractice_20200615.utils.ServerUtil
 
 class LoginActivity : BaseActivity() {
 
@@ -19,6 +20,8 @@ class LoginActivity : BaseActivity() {
             val pw = passwordEdt.text.toString()
 
 //            입력한 ID / PW가 진짜 회원인지 서버에 물어봐야함 (요청 필요)
+
+            ServerUtil.postRequestLogin(mContext, email, pw, null)
 
         }
 
