f9bab406357334f571d657c6e6ab20be6f2e296e
cho881020 <kj_cho@idealidea.co.kr>
2020-06-16 21:31:43 +0900

    메인화면에서 내 정보 API 호출

--- app/src/main/java/kr/co/tjoeun/apipractice_20200615/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/apipractice_20200615/MainActivity.kt
@@ -2,6 +2,8 @@ package kr.co.tjoeun.apipractice_20200615
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kr.co.tjoeun.apipractice_20200615.utils.ServerUtil
+import org.json.JSONObject
 
 class MainActivity : BaseActivity() {
 
@@ -17,6 +19,14 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+//        로그인한 사용자 정보를 서버에서 불러오기.
+        ServerUtil.getRequestUserInfo(mContext, object : ServerUtil.JsonResponseHandler {
+            override fun onResponse(json: JSONObject) {
+
+            }
+
+        })
 
     }
 
