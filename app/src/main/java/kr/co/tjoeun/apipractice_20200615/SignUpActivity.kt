30b8d3323d35c011ea971e38f3897407d829eb2f
cho881020 <kj_cho@idealidea.co.kr>
2020-06-17 21:01:27 +0900

    이메일 입력값이 변경되면 => 재검사 요구

--- app/src/main/java/kr/co/tjoeun/apipractice_20200615/SignUpActivity.kt
+++ app/src/main/java/kr/co/tjoeun/apipractice_20200615/SignUpActivity.kt
@@ -2,6 +2,9 @@ package kr.co.tjoeun.apipractice_20200615
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_sign_up.*
 import kr.co.tjoeun.apipractice_20200615.utils.ServerUtil
@@ -22,6 +25,29 @@ class SignUpActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+
+//        이메일 입력값이 변경되면 => 무조건 다시 검사를 받으라고 문구 / Boolean 변경
+
+        emailEdt.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+//            문구가 바꼈을때
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+//                이메일 검사 결과를 => 검사 필요 문구로.
+                emailCheckResultTxt.text = "이메일 중복검사를 해주세요."
+//                  이메일 중복검사를 다시 해야하므로 => 사용 불가로 처리
+                isEmailOk = false
+
+            }
+
+        })
 
         signUpBtn.setOnClickListener {
 
