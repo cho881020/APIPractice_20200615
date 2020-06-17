9a43726e1414ee5fb539abe0bd2f46b938def62e
cho881020 <kj_cho@idealidea.co.kr>
2020-06-17 22:04:19 +0900

    상세화면에서 => 진행현황 불러오기  => Json파싱 / 화면반영 X

--- app/src/main/java/kr/co/tjoeun/apipractice_20200615/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/apipractice_20200615/MainActivity.kt
@@ -29,7 +29,11 @@ class MainActivity : BaseActivity() {
     override fun setupEvents() {
 
         topicListView.setOnItemClickListener { parent, view, position, id ->
+
+            val clickedTopic = topicList[position]
+
             val myIntent = Intent(mContext, ViewTopicDetailActivity::class.java)
+            myIntent.putExtra("topic_id", clickedTopic.id)
             startActivity(myIntent)
         }
 
