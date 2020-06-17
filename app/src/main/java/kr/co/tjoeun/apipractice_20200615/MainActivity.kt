e5aabc62800b2f582f41ea3035457630ddacf2ab
cho881020 <kj_cho@idealidea.co.kr>
2020-06-17 19:40:49 +0900

    리스트뷰 / 어댑터 연결 + 서버에서 받아온 내용 새로고침

--- app/src/main/java/kr/co/tjoeun/apipractice_20200615/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/apipractice_20200615/MainActivity.kt
@@ -3,6 +3,7 @@ package kr.co.tjoeun.apipractice_20200615
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
+import kr.co.tjoeun.apipractice_20200615.adapters.TopicAdapter
 import kr.co.tjoeun.apipractice_20200615.datas.Topic
 import kr.co.tjoeun.apipractice_20200615.datas.User
 import kr.co.tjoeun.apipractice_20200615.utils.ServerUtil
@@ -11,6 +12,8 @@ import org.json.JSONObject
 class MainActivity : BaseActivity() {
 
     val topicList = ArrayList<Topic>()
+
+    lateinit var topicAdapter : TopicAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,6 +27,10 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+        topicAdapter = TopicAdapter(mContext, R.layout.topic_list_item, topicList)
+        topicListView.adapter = topicAdapter
+
 //        진행중인 토론 목록이 어떤게 있는지? 서버에 물어보자.
         ServerUtil.getRequestMainInfo(mContext, object : ServerUtil.JsonResponseHandler {
             override fun onResponse(json: JSONObject) {
@@ -41,6 +48,9 @@ class MainActivity : BaseActivity() {
 //                    받아온 주제 목록을 리스트뷰의 재료로 추가
                     topicList.add(topic)
                 }
+
+//                리스트뷰의 내용 추가
+                topicAdapter.notifyDataSetChanged()
 
             }
 
