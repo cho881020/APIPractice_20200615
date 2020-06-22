e3096e7bcd4bf2ccc1fe1cdff85394d34c800b22
cho881020 <kj_cho@idealidea.co.kr>
2020-06-22 21:54:54 +0900

    좋아요/싫어요 갯수를 바로바로 변경

--- app/src/main/java/kr/co/tjoeun/apipractice_20200615/adapters/TopicReplyAdapter.kt
+++ app/src/main/java/kr/co/tjoeun/apipractice_20200615/adapters/TopicReplyAdapter.kt
@@ -1,6 +1,8 @@
 package kr.co.tjoeun.apipractice_20200615.adapters
 
 import android.content.Context
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -79,6 +81,24 @@ class TopicReplyAdapter(
 
             ServerUtil.postRequestReplyLikeOrDislike(mContext, data.id, isLike, object : ServerUtil.JsonResponseHandler {
                 override fun onResponse(json: JSONObject) {
+
+//                    목록을 뿌릴때 쓰는 data와 이름이 겹쳐서 변경 (dataObj) => data도 활용 예정
+                    val dataObj = json.getJSONObject("data")
+                    val reply = dataObj.getJSONObject("reply")
+
+//                    data변수 내부 값중 좋아요 /싫어요 갯수 변경
+                    data.likeCount = reply.getInt("like_count")
+                    data.dislikeCount = reply.getInt("dislike_count")
+
+//                    리스트뷰에 뿌려지는 데이터에 내용 변경 => notifyDataSetChanged 필요
+//                    어댑터변수.notify~ 실행. 어댑터변수 X.
+
+//                    어댑터 내부에서는 직접 새로고침 가능 => runOnUiThread 필요
+
+//                    runUiThread 대체재
+                    Handler(Looper.getMainLooper()).post {
+                        notifyDataSetChanged()
+                    }
 
                 }
             })
