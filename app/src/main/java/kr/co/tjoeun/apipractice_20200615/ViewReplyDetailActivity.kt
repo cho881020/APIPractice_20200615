59973cd0cb0a6d5ca764b59e5abd904ac78fa5ab
cho881020 <kj_cho@idealidea.co.kr>
2020-06-23 21:01:55 +0900

    의견 작성 내용 서버에서 불러와서 반영

--- app/src/main/java/kr/co/tjoeun/apipractice_20200615/ViewReplyDetailActivity.kt
+++ app/src/main/java/kr/co/tjoeun/apipractice_20200615/ViewReplyDetailActivity.kt
@@ -2,6 +2,8 @@ package kr.co.tjoeun.apipractice_20200615
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_view_reply_detail.*
+import kr.co.tjoeun.apipractice_20200615.datas.TopicReply
 import kr.co.tjoeun.apipractice_20200615.utils.ServerUtil
 import org.json.JSONObject
 
@@ -9,6 +11,9 @@ class ViewReplyDetailActivity : BaseActivity() {
 
 //    어떤 의견에 대해 보러온건지
     var mReplyId = -1
+
+//    서버에서 받아온 의견 저장
+    lateinit var mReply : TopicReply
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,8 +39,17 @@ class ViewReplyDetailActivity : BaseActivity() {
         ServerUtil.getRequestReplyDetail(mContext, mReplyId, object : ServerUtil.JsonResponseHandler {
             override fun onResponse(json: JSONObject) {
 
-            }
+                val data = json.getJSONObject("data")
+                val reply = data.getJSONObject("reply")
+                mReply = TopicReply.getTopicReplyFromJson(reply)
 
+                runOnUiThread {
+                    contentTxt.text = mReply.content
+                    writerNickNameTxt.text = mReply.writer.nickName
+                    selectedSideTitleTxt.text = "(${mReply.selectedSide.title})"
+                }
+
+            }
         })
     }
 
